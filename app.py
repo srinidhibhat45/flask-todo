14a6e8f51b84f1ac5fb66d3482efc32db28e0bcb
Srinidhi Bhat <srinidhibhat45@gmail.com>
2021-05-26 22:55:12 +0530

    major changes

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -17,31 +16,40 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    if request.method == 'POST':
+    if request.method=='POST':
         title = request.form['title']
         desc = request.form['desc']
         todo = Todo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
-    allTodo = Todo.query.all()
+        
+    allTodo = Todo.query.all() 
     return render_template('index.html', allTodo=allTodo)
 
 
-@app.route('/update')
-def update():
-    return "This is Products page!"
-
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+        
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
     todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect('/')
+    return redirect("/")
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
